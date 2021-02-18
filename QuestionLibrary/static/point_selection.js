6631fcaa2ced63bd85fcfc28a91fcdf3b0175824
Max <jmaxmarno@gmail.com>
2021-02-17 21:21:30 -0700

    update map and available features on feature layer toggle

--- QuestionLibrary/static/point_selection.js
+++ QuestionLibrary/static/point_selection.js
@@ -24,130 +24,153 @@
                 tempGraphicsLayer.listMode = 'hide';
                 var references = new VectorTileLayer("https://www.arcgis.com/sharing/rest/content/items/af6063d6906c4eb589dfe03819610660/resources/styles/root.json");
                 references.listMode = 'hide';
+                var base_service = document.getElementById('id_base_map_service').value;
+                const fl_node = document.getElementById('id_layer');
+
                 var editGraphic;
                 var allFeatures = [];
-
-
-
-                //var projectionPromise = projection.load();
+                let flayers = [];
+                let fl;
+                let view;
+                parseLayers();
+                renderMap();
 
                 // Create a MapView instance (for 2D viewing) and reference the map instance
-
-                const fl_node = document.getElementById('id_layer');
                 fl_node.onchange = changeFeatureLayer;
                 function changeFeatureLayer() {
-                    console.log(fl_node.value);
-                };
-
-
-                if (document.getElementById('id_base_map_service').value) {
-                    var base_service = document.getElementById('id_base_map_service').value;
-                    // {# todo: figure out how to id the correct layer #}
-                    var fl = new FeatureLayer({
-                        url: base_service + "/1",
-                        title: 'Facility Service'
-                    });
-                    var fl_0 = new FeatureLayer({
-                        url: base_service + "/0",
-                        title: 'Base Inventory'
-                    });
-                    // Create a Map instance
-                    var myMap = new Map({
-                        basemap: 'satellite',
-                        layers: [references, fl, fl_0, tempGraphicsLayer]
-                    });
-                    var view = new MapView({
-                        map: myMap,
-                        container: 'mapDiv'
-                    });
-                    fl.when(function () {
-                        view.extent = fl.fullExtent;
-                        getFeatures();
-                    });
-                    var layerlist = new LayerList({
-                      view: view
-                    });
-                    const layerListExpand = new Expand({
-                        expandIconClass: "esri-icon-layer-list",
-                        view: view,
-                        content: layerlist
-                    });
-                    view.ui.add(layerListExpand, "top-left");
-
-                    const sketch = new Sketch({
-                        layer: tempGraphicsLayer,
-                        view: view,
-                        availableCreateTools: ["polygon", "rectangle", "circle"]
-                        // graphic will be selected as soon as it is created
-                        //creationMode: "update"
-                    });
-
-                    /* once FeatureTable has more features and is out of beta it might be worth it
-                    const featureTable = new FeatureTable({
-                      view: view, // The view property must be set for the select/highlight to work
-                      layer: fl,
-                      container: "featuresTable"
-                    });*/
-
-                    sketch.on('update', e => {
-                        $('#mapDiv button:not([type])').attr('type', 'button');
-                        if (e.state === 'complete' && !e.aborted) {
-                            selectFeaturesByGeometry(e.graphics[0].geometry, false);
-                            selectFeaturesByGeometry(e.graphics[0].geometry, true);
-                        }
-                    });
-                    sketch.on('create', e => {
-                        if (e.state === 'complete') {
-                            selectFeaturesByGeometry(e.graphic.geometry, false);
-                        }
-                    });
-                    sketch.on('delete', e => {
-                        selectFeaturesByGeometry(e.graphics[0].geometry, true);
-                    });
-                    const fullscreen = new Fullscreen({
-                        view: view
-                    });
-                    view.ui.add(sketch, "top-right");
-                    view.on("immediate-click", event => {
-                        view.hitTest(event).then(response => {
-                            const candidate = response.results.find(result => {
-                                return (
-                                    result.graphic &&
-                                    result.graphic.layer &&
-                                    result.graphic.layer === fl
-                                );
-                            });
-                            // Select the rows of the clicked feature
-                            var current_selection = getCurrentSelection();
-                            var remove = current_selection.includes(candidate.graphic.attributes.OBJECTID);
-                            candidate && selectFeatures([candidate.graphic.attributes.OBJECTID], remove);
-                        });
-                    });
-                    view.when(function () {
-                        $('#mapDiv button:not([type])').attr('type', 'button');
-                        //$('#featuresTable button:not([type])').attr('type', 'button');
-                        // apears to select by objectid
-                        //featureTable.selectRows([336, 326]);
-                    });
-                    view.whenLayerView(fl).then(layerView => console.log(layerView));
+                    // update focus fl
+                    // redraw map, clear selection (get current)
+                    // redraw table
+                    renderMap();
                 }
+                function renderMap() {
+                    if (document.getElementById('id_base_map_service').value) {
+                        let fl = flayers[fl_node.options.selectedIndex];
+                        // var fl = new FeatureLayer({
+                        //     url: base_service + "/1"
+                        // });
+
+                        // Create a Map instance
+                        const mapDiv = document.getElementById('mapDiv');
+                        mapDiv.innerHTML = "";
+                        var myMap = new Map({
+                            basemap: 'satellite',
+                            layers:  flayers.concat([references, tempGraphicsLayer])
+                        });
+                        view = new MapView({
+                            map: myMap,
+                            container: 'mapDiv'
+                        });
+                        fl.when(function () {
+                            view.extent = fl.fullExtent;
+                            getFeatures(fl);
+                        });
+                        var layerlist = new LayerList({
+                          view: view,
+                            // listItemCreatedFunction: function(event) {
+                            //   var item = event.item;
+                            //   item.title = item.title.replace('WET_Tool_BASE', '');
+                            // }
+                        });
+                        const layerListExpand = new Expand({
+                            expandIconClass: "esri-icon-layer-list",
+                            view: view,
+                            content: layerlist
+                        });
+                        view.ui.add(layerListExpand, "top-left");
+
+                        const sketch = new Sketch({
+                            layer: tempGraphicsLayer,
+                            view: view,
+                            availableCreateTools: ["polygon", "rectangle", "circle"]
+                            // graphic will be selected as soon as it is created
+                            //creationMode: "update"
+                        });
+
+                        /* once FeatureTable has more features and is out of beta it might be worth it
+                        const featureTable = new FeatureTable({
+                          view: view, // The view property must be set for the select/highlight to work
+                          layer: fl,
+                          container: "featuresTable"
+                        });*/
+
+                        sketch.on('update', e => {
+                            $('#mapDiv button:not([type])').attr('type', 'button');
+                            if (e.state === 'complete' && !e.aborted) {
+                                selectFeaturesByGeometry(e.graphics[0].geometry, false);
+                                selectFeaturesByGeometry(e.graphics[0].geometry, true);
+                            }
+                        });
+                        sketch.on('create', e => {
+                            if (e.state === 'complete') {
+                                selectFeaturesByGeometry(e.graphic.geometry, false);
+                            }
+                        });
+                        sketch.on('delete', e => {
+                            selectFeaturesByGeometry(e.graphics[0].geometry, true);
+                        });
+                        const fullscreen = new Fullscreen({
+                            view: view
+                        });
+                        view.ui.add(sketch, "top-right");
+                        view.on("immediate-click", event => {
+                            view.hitTest(event).then(response => {
+                                const candidate = response.results.find(result => {
+                                    return (
+                                        result.graphic &&
+                                        result.graphic.layer &&
+                                        result.graphic.layer === fl
+                                    );
+                                });
+                                // Select the rows of the clicked feature
+                                var current_selection = getCurrentSelection();
+                                var remove = current_selection.includes(candidate.graphic.attributes.OBJECTID);
+                                candidate && selectFeatures([candidate.graphic.attributes.OBJECTID], remove);
+                            });
+                        });
+                        view.when(function () {
+                            $('#mapDiv button:not([type])').attr('type', 'button');
+                            //$('#featuresTable button:not([type])').attr('type', 'button');
+                            // apears to select by objectid
+                            //featureTable.selectRows([336, 326]);
+                        });
+                        view.whenLayerView(fl).then(layerView => console.log(layerView));
+                    }
+                }
+
+
+
 
                 var gridOptions;
 
-                function getFeatures() {
-                    fl.queryFeatures({"where": "1=1", "outFields": "*"}).then(function (featureSet) {
+                function parseLayers() {
+                    flayers = [];
+                    for (let i=0; i<fl_node.options.length; i++){
+                        const ii = (i).toString();
+                        const f = new FeatureLayer({
+                            url: base_service + "/"+ii,
+                            title: fl_node.options[i].innerText
+                        });
+                        flayers.push(f);
+                    }
+                //     todo - update layer list
+                }
+
+                function getFeatures(featureLayer) {
+                    featureLayer.queryFeatures({"where": "1=1", "outFields": "*"}).then(function (featureSet) {
                         // todo: deal with getting more features if max returned
                         allFeatures = allFeatures.concat(featureSet.features);
                         var features = featureSet.features.map(f => f.attributes);
+                        // todo - remove unnecessary fields from Base Inventory if Base Inventory
                         var columnDefs = featureSet.fields
                         //.filter(f => ! ['OBJECTID','created_date', 'created_user', 'last_edited_user', 'last_edited_date'].includes(f.name))
                             .map(f => {
                                 var columnDef = {"headerName": f.alias, "field": f.name};
                                 if (['OBJECTID', 'created_date', 'created_user', 'last_edited_user', 'last_edited_date'].includes(f.name)) {
-                                    columnDef['hide'] = true
+                                    columnDef['hide'] = true;
                                 }
                                 return columnDef;
-                            })
+                            });
                         gridOptions = {
                             defaultColDef: {
                                 flex: 1,
@@ -164,42 +187,38 @@
                                 highlightFeatures();
                             },
                             onRowClicked: function (e) {
-                                selectFeatures([e.node.data.OBJECTID], !e.node.isSelected())
+                                selectFeatures([e.node.data.OBJECTID], !e.node.isSelected());
                             }
-                        }
-                        var gridDiv = document.querySelector('#featuresTable');
-                        new agGrid.Grid(gridDiv, gridOptions);
+                        };
+                        let gridDiv = document.querySelector('#featuresTable');
+                        gridDiv.innerHTML = "";
+                        let aGrid = new agGrid.Grid(gridDiv, gridOptions);
 
                     });
 
                 }
 
                 function selectFeaturesByGeometry(geometry, remove) {
-                    fl.queryObjectIds({geometry, spatialRelationship: 'contains'}).then(function (ids) {
+                    flayers[fl_node.options.selectedIndex].queryObjectIds({geometry, spatialRelationship: 'contains'}).then(function (ids) {
                         selectFeatures(ids, remove);
                     });
                 }
 
                 function selectFeatures(ids, remove) {
                     var current_selection = getCurrentSelection();
-                    if (remove) {
-                        var new_selection = current_selection.filter(id => !ids.includes(id))
-                    } else {
-                        var new_selection = new Set(current_selection.concat(ids));
-                    }
+                    var new_selection = remove ? current_selection.filter(id => !ids.includes(id)): new Set(current_selection.concat(ids));
                     document.getElementById('id_selected_features').value = [...new_selection].join(',');
                     highlightFeatures();
                 }
 
                 var highlight;
-
                 function highlightFeatures() {
                     var current_selection = getCurrentSelection();
                     gridOptions.api.forEachNode(function (node) {
                         node.setSelected(current_selection.includes(node.data.OBJECTID));
                     });
 
-                    view.whenLayerView(fl).then(layerView => {
+                    view.whenLayerView(flayers[fl_node.options.selectedIndex]).then(layerView => {
                         if (highlight) {
                             highlight.remove();
                         }
@@ -212,9 +231,8 @@
                     return document.getElementById('id_selected_features').value
                         .split(',')
                         .filter(id => id !== "")
-                        .map(id => parseInt(id, 10))
+                        .map(id => parseInt(id, 10));
                 }
-
             });
     });
 })(django.jQuery);
