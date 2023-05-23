69b9c6c5492b3cdf9883133e7993558141190c6e
Travis <tbock@innovateteam.com>
2023-05-23 13:42:50 -0700

    wrong logger?

--- QuestionLibrary/views.py
+++ QuestionLibrary/views.py
@@ -18,7 +18,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 import logging
-logger  = logging.getLogger(__name__)
+logger = logging.getLogger('django')
 
 @method_decorator(login_required, name='dispatch')
 class EsriProxy(View):
